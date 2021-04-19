5c2c5474e13cb15fbabcaf301db86607bc53a8af
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-19 11:45:20 +0200

    Update pythonfetch.py

--- pythonfetch.py
+++ pythonfetch.py
@@ -1,5 +1,15 @@
 import os, sys
 
-print('OS:', os.uname()[0], os.uname()[1])
-print('Encoding:', os.device_encoding(0))
-print('CPU number:', os.cpu_count())
+osname = os.uname()[0] + os.uname()[1]
+kernel = os.uname()[2]
+arch = os.uname()[3]
+encoding = os.device_encoding(0)
+pythonv = sys.version[0] + sys.version[1] + sys.version[2] + sys.version[3] + sys.version[4] + sys.version[5]
+cpu_number = os.cpu_count()
+
+print('OS:', osname)
+print('Architecture:', arch)
+print('Kernel:', kernel)
+print('Encoding:', encoding)
+print('Python version:', pythonv)
+print('CPU number:', cpu_number)
