788534c988b196303cc5bbc2d2c07c8c1e7889cf
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-19 11:47:25 +0200

    Update pythonfetch.py

--- pythonfetch.py
+++ pythonfetch.py
@@ -2,7 +2,7 @@ import os, sys
 
 osname = os.uname()[0] + os.uname()[1]
 kernel = os.uname()[2]
-arch = os.uname()[3]
+arch = os.uname()[4]
 encoding = os.device_encoding(0)
 pythonv = sys.version[0] + sys.version[1] + sys.version[2] + sys.version[3] + sys.version[4] + sys.version[5]
 cpu_number = os.cpu_count()
