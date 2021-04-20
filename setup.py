ec39d70ad47733bb93fd0aad39267f1fe314a2d9
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-20 21:52:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ def help():
 
 try:
     if sys.argv[1] == 'install':
+        print('Installing dependencies...')
+        os.system('pip install --user psutil')
         print('Installing pyfetch...')
         os.system('cp pyfetch.py /usr/bin/pyfetch')
         print('Changing permission for /usr/bin/pyfetch...')
@@ -14,6 +16,8 @@ try:
     elif sys.argv[1] == 'uninstall':
         print('Uninstalling pyfetch...')
         os.system('rm -f /usr/bin/pyfetch')
+        if input('Uninstall psutil? [y/N]) == 'y':
+                 os.system('pip uninstall psutil')
     else:
         print('Unknown options:', sys.argv[1])
         help()
