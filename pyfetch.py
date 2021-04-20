24d9bd923cfed1636c2ea965a0e9139d66edbb3f
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-20 16:31:35 +0200

    Update pyfetch.py

--- pyfetch.py
+++ pyfetch.py
@@ -1,5 +1,6 @@
 import os, sys
 
+username = os.environ['USER']
 osname = os.uname()[0] + ' ' + os.uname()[1]
 arch = os.uname()[4]
 kernel = os.uname()[2]
@@ -17,14 +18,20 @@ def uptime():
     if m > 1:
         uptime += str(m) + ' mins '
     return uptime
+shell = os.environ['SHELL']
+editor = os.environ['EDITOR']
+lang = os.environ['LANGUAGE']
 encoding = os.device_encoding(0)
 pythonv = sys.version[0] + sys.version[1] + sys.version[2] + sys.version[3] + sys.version[4] + sys.version[5]
 cpu_number = os.cpu_count()
 
-print('OS:', osname)
-print('Architecture:', arch)
+print()
+print('OS:', osname, arch)
 print('Kernel:', kernel)
 print('Uptime: %s' % uptime())
+print('Shell:', shell)
+print('Editor:', editor)
+print('Language:', lang)
 print('Encoding:', encoding)
 print('Python version:', pythonv)
 print('CPU number:', cpu_number)
