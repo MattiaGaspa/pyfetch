2ea5ebd31ebd9eaf62843188608c39d035df5829
gasmat <53443116+MattiaG-afk@users.noreply.github.com>
2021-04-20 22:36:12 +0200

    Update pyfetch.py

--- pyfetch.py
+++ pyfetch.py
@@ -37,16 +37,45 @@ def convert(bytes):
         unitIndex += 1
     return f"{round(bytes)}{units[unitIndex]}"
 
+color = {
+    'normal': '\u001b[00;0m',
+    'gentoo': '\u001b[35;1m'
+}
+logo = {
+    'gentoo': [
+        '         \u001b[35;1m-/oyddmdhs+:.\u001b[00;1m\t\t\t',
+        '     \u001b[35;1m-o\u001b[00;1mdNMMMMMMMMNNmhy+\u001b[35;1m-`\u001b[00;0m\t\t',
+        '   \u001b[35;1m-y\u001b[00;1mNMMMMMMMMMMMNNNmmdhy\u001b[35;1m+-\u001b[00;0m\t\t',
+        ' \u001b[35;1m`o\u001b[00;1mmMMMMMMMMMMMMNmdmmmmddhhy\u001b[35;1m/`\u001b[00;0m\t\t',
+        ' \u001b[35;1mom\u001b[00;1mMMMMMMMMMMMN\u001b[35;1mhhyyyo\u001b[00;1mhmdddhhhd\u001b[35;1mo`\u001b[00;0m\t',
+        '\u001b[35;1m.y\u001b[00;1mdMMMMMMMMMMd\u001b[35;1mhs++so/s\u001b[00;1mmdddhhhhdm\u001b[35;1m+`\u001b[00;0m\t',
+        ' \u001b[35;1moy\u001b[00;1mhdmNMMMMMMMN\u001b[35;1mdyooy\u001b[00;1mdmddddhhhhyhN\u001b[35;1md.\u001b[00;0m\t',
+        '  \u001b[35;1m:o\u001b[00;1myhhdNNMMMMMMMNNNmmdddhhhhhyym\u001b[35;1mMh\u001b[00;0m\t',
+        '    \u001b[35;1m.:\u001b[00;1m+sydNMMMMMNNNmmmdddhhhhhhmM\u001b[35;1mmy\u001b[00;0m\t',
+        '       \u001b[35;1m/m\u001b[00;1mMMMMMMNNNmmmdddhhhhhmMNh\u001b[35;1ms:\u001b[00;0m\t',
+        '    \u001b[35;1m`o\u001b[00;1mNMMMMMMMNNNmmmddddhhdmMNhs\u001b[35;1m+`\u001b[00;0m\t',
+        '  \u001b[35;1m`s\u001b[00;1mNMMMMMMMMNNNmmmdddddmNMmhs\u001b[35;1m/.\u001b[00;0m\t',
+        ' \u001b[35;1m/N\u001b[00;1mMMMMMMMMNNNNmmmdddmNMNdso\u001b[35;1m:`\u001b[00;0m\t\t',
+        '\u001b[35;1m+M\u001b[00;1mMMMMMMNNNNNmmmmdmNMNdso\u001b[35;1m/-\u001b[00;0m\t\t',
+        '\u001b[35;1myM\u001b[00;1mMNNNNNNNmmmmmNNMmhs+/\u001b[35;1m-`\u001b[00;0m\t\t',
+        '\u001b[35;1m/h\u001b[00;1mMMNNNNNNNNMNdhs++/\u001b[35;1m-`\u001b[00;0m\t\t\t',
+        '\u001b[35;1m`/\u001b[00;1mohdmmddhys+++/:\u001b[35;1m.`\u001b[00;0m\t\t\t',
+        '  \u001b[35;1m`-//////:--.\u001b[00;1m\t\t\t\t'
+    ]
+}
+
 print(username + '@' + os.uname()[1])
 print('--------------------------')
-print('OS:', osname, arch)
-print('Kernel:', kernel)
-print('Uptime: %s' % uptime())
-print('Shell:', shell)
-print('Editor:', editor)
-print('Language:', lang)
-print('Encoding:', encoding)
-print('Python version:', pythonv)
-print('CPU number:', cpu_number)
-print('Memory:', convert(used_ram) + '/' + convert(ram))
-print('Swap:', convert(used_swap) + '/' + convert(swap))
+print(logo[os.uname()[1]][0] + '|\t' + color[os.uname()[1]] + 'OS' + color["normal"] + ':', osname, arch)
+print(logo[os.uname()[1]][1] + '|\t' + color[os.uname()[1]] + 'Kernel' + color["normal"] + ':', kernel)
+print(logo[os.uname()[1]][2] + '|\t' + color[os.uname()[1]] + 'Uptime' + color["normal"] + ': %s' % uptime())
+print(logo[os.uname()[1]][3] + '|\t' + color[os.uname()[1]] + 'Shell' + color["normal"] + ':', shell)
+print(logo[os.uname()[1]][4] + '|\t' + color[os.uname()[1]] + 'Editor' + color["normal"] + ':', editor)
+print(logo[os.uname()[1]][5] + '|\t' + color[os.uname()[1]] + 'Language' + color["normal"] + ':', lang)
+print(logo[os.uname()[1]][6] + '|\t' + color[os.uname()[1]] + 'Encoding' + color["normal"] + ':', encoding)
+print(logo[os.uname()[1]][7] + '|\t' + color[os.uname()[1]] + 'Python version' + color["normal"] + ':', pythonv)
+print(logo[os.uname()[1]][8] + '|\t' + color[os.uname()[1]] + 'CPU number' + color["normal"] + ':', cpu_number)
+print(logo[os.uname()[1]][9] + '|\t' + color[os.uname()[1]] + 'Memory' + color["normal"] + ':', convert(used_ram) + '/' + convert(ram))
+print(logo[os.uname()[1]][10] + '|\t' + color[os.uname()[1]] + 'Swap' + color["normal"] + ':', convert(used_swap) + '/' + convert(swap))
+for i in range(11, len(logo[os.uname()[1]])):
+    print(logo[os.uname()[1]][i])
